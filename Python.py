924f83738903152ecd591080d74c5cbcc1ff37da
miaodongfang <miaodongfang@ppdai.com>
2016-11-15 14:58:40 +0800

    ..

--- Python.py
+++ Python.py
@@ -1,3 +1,22 @@
-hello~
+# -*- coding: utf-8 -*-
 
-this is a test file 
+def my_abs(x):
+    if x >=0:
+        return x
+    elif x<0:
+        return -x
+
+
+n1 = 255
+n2 = 1000
+print(hex(n1))
+print(hex(n2))
+
+def my_abs(x):
+    if not isinstance(x, (int, float)):
+        raise TypeError('bad operand type')
+    if x >= 0:
+        return x
+    else:
+        return -x
+print my_abs('f')
