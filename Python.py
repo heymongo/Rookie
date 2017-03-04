b6ee1ad2034986727e4eebbf64e690c8377c11b5
miaodongfang <miaodongfang@ppdai.com>
2017-03-04 14:13:12 +0800

    20170303

--- Python.py
+++ Python.py
@@ -312,7 +312,7 @@
 #print prod([1,2,3,4])
 
 #Python内建的filter()函数用于过滤序列.和map()类似，filter()也接收一个函数和一个序列。
-# 和map()不同的时，filter()把传入的函数依次作用于每个元素，然后根据返回值是True还是False决定保留还是丢弃该元素
+# 和map()不同的是，filter()把传入的函数依次作用于每个元素，然后根据返回值是True还是False决定保留还是丢弃该元素
 # 1.在一个list中删掉偶数，只保留奇数，如下：n%2==1取奇数，n%2==0取偶数
 def is_odd(n):
     return n % 2 == 0
@@ -323,10 +323,13 @@ def not_empty(s):
     return s and s.strip()
 print filter(not_empty, ['A ', '', ' ','aa',None])
 
+def f(x):
+    return x*x
+r = map(f,[1,2,3,4,5,6])
+print list(r)
 
 
+from functools import reduce
 
-
-
-
-
+def add(x,y):
+    return x+y
